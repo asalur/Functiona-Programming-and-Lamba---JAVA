6aa406586bff8928fa11671a2792943b4d83ecdd
Pedro Ruiz Marrara <pedrormarrara@gmail.com>
2022-11-07 16:55:56 +0000

    Predicate Example

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,20 +1,30 @@
 package app;
-import java.util.Arrays;
+
+import entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class Program {
-    public static int globalValue = 3;
-
+    public static int compareProducts(Product p1, Product p2) {
+        return p1.getPrice().compareTo(p2.getPrice());
+    }
     public static void main(String[] args) {
 
-        int[] vect = new int[] {3, 4, 5};
-        changeOddValues(vect);
-        System.out.println(Arrays.toString(vect));
-    }
-    public static void changeOddValues(int[] numbers) {
-        for (int i=0; i<numbers.length; i++) {
-            if (numbers[i] % 2 != 0) {
-                numbers[i] += globalValue;
-            }
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.00));
+        list.add(new Product("HD Case", 90.00));
+
+        Predicate<Product> pred = p -> p.getPrice() >= 100.00;
+
+        list.removeIf(pred);
+
+        for(Product p : list){
+            System.out.println(p);
         }
     }
 }
