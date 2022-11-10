2ea82f31b4daa49ef716f8d08afdbd0a3d33b3a8
Pedro Ruiz Marrara <pedrormarrara@gmail.com>
2022-11-10 11:39:49 +0000

    Class function - map method

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,12 +1,11 @@
 package app;
 
 import entities.Product;
-import util.PriceUpdate;
+import util.UpperCaseName;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Program {
     public static int compareProducts(Product p1, Product p2) {
@@ -21,12 +20,9 @@ public class Program {
         list.add(new Product("Tablet", 350.00));
         list.add(new Product("HD Case", 90.00));
 
+        List <String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
 
-        double factor = 1.1;
+        names.forEach(System.out :: println);
 
-
-        list.forEach(p -> p.setPrice(p.getPrice() * factor);
-
-        list.forEach(System.out :: println);
     }
 }
