5333c1e72f373cc3a60b24143269c1591e640f4a
Pedro Ruiz Marrara <pedrormarrara@gmail.com>
2022-11-10 11:44:20 +0000

    Reference Method - Static Method - map method

--- src/app/Program.java
+++ src/app/Program.java
@@ -20,7 +20,7 @@ public class Program {
         list.add(new Product("Tablet", 350.00));
         list.add(new Product("HD Case", 90.00));
 
-        List <String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+        List <String> names = list.stream().map(Product :: staticUpperCaseName).collect(Collectors.toList());
 
         names.forEach(System.out :: println);
 
