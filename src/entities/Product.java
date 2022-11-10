8bac947fe26cc412a5628dd5f381a1ca11c669ce
Pedro Ruiz Marrara <pedrormarrara@gmail.com>
2022-11-10 11:45:38 +0000

    Reference Method - Non Static Method - map method

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -30,6 +30,11 @@ public class Product {
         return p.getName().toUpperCase();
    }
 
+    public String nonStaticUpperCaseName(){
+        return name.toUpperCase();
+    }
+
+
     @Override
     public String toString() {
         return name + ", " + String.format("%.2f", price);
