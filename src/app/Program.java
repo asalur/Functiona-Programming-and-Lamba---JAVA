fe21d78eda9c7391d069f3ced4f2d4d07b88cf41
Pedro Ruiz Marrara <pedrormarrara@gmail.com>
2022-11-10 13:23:03 +0000

    lambda inline - predicate as argument

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,12 +1,10 @@
 package app;
 
 import entities.Product;
-import util.UpperCaseName;
+import module.services.ProductService;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.PropertyPermission;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Program {
@@ -21,11 +19,11 @@ public class Program {
         list.add(new Product("Mouse", 50.00));
         list.add(new Product("Tablet", 350.00));
         list.add(new Product("HD Case", 90.00));
-        
 
-        List <String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
+        ProductService ps = new ProductService();
 
-        names.forEach(System.out :: println);
+        double sum = ps.filterSum(list, p -> p.getPrice() < 100.0 );
+        System.out.println("Sum = " + String.format("%.2f", sum));
 
     }
 }
